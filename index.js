29bec106fca4294bc8089831de42a42bae8c2d21
Josmar Dias <josmarnet@gmail.com>
2016-04-27 00:39:39 -0300

    remove async support
    async/await is not on the ECMA2015 scope, so it doesn't make sense
    to include it on a ECMA2015 preset for Node 6.x

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 /* global module */
 module.exports = {
   plugins: [
-    require('babel-plugin-syntax-async-functions'),
     require('babel-plugin-syntax-trailing-function-commas'),
     require('babel-plugin-transform-es2015-modules-commonjs'),
-    require('babel-plugin-transform-async-to-generator'),
   ],
 };
